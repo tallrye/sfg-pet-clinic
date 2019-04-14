ad3a3f5cc63905a6aefe1565891603e6b217005f
tallrye <s_uzuncavdar@hotmail.com>
2019-04-14 21:07:44 +0300

    Packaging

---
+++ pet-clinic-data/src/main/java/com/thinkerfox/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.thinkerfox.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ pet-clinic-data/src/main/java/com/thinkerfox/sfgpetclinic/model/Vet.java
@@ -0,0 +1,6 @@
+package com.thinkerfox.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+
+}
